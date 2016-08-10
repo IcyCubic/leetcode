99899343abf37e3d0ffe14662e5140d4e255fec0
Henry Lai <HLAware@gmail.com>
2016-08-10 00:26:38 -0700

    Solution to question 001, Two Sum

--- LeetCode/src/questions/Two_Sum.java
+++ LeetCode/src/questions/Two_Sum.java
@@ -1,5 +1,21 @@
 package questions;
 
 public class Two_Sum {
-
+    public int[] twoSum(int[] nums, int target) {
+        for (int first = 0, max=nums.length; first < max; first++){
+            for (int second = first + 1; second < max; second++){
+                if (nums[first] + nums[second] == target){	                    
+                	return new int[] {first, second};
+                }
+            }
+        }
+    return null;
+    }
+    
+    public static void main(String[] args){
+    	Two_Sum ts = new Two_Sum();
+    	int[] solution = ts.twoSum(new int[]{3,2,4}, 6);
+    	System.out.printf("[%d,%d]", solution[0], solution[1]);
+    	
+    }
 }
