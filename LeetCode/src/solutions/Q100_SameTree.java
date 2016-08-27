8847a0969fd64562acdcd2653bdabaa118d3072b
Henry Lai <HLAware@gmail.com>
2016-08-26 22:46:12 -0700

    Question 100 Same Tree solution (recursive)
    Also added TreeNode.java as a supporting class

---
+++ LeetCode/src/solutions/Q100_SameTree.java
@@ -0,0 +1,26 @@
+package solutions;
+/**
+Given two binary trees, write a function to check if they are equal or not.
+
+Two binary trees are considered equal if they are structurally identical 
+and the nodes have the same value. 
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Q100_SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p != null && 
+    		q != null && 
+    		p.val == q.val &&
+    		isSameTree(p.left, q.left) &&
+    		isSameTree(p.right, q.right)) return true;
+    	
+    	return false;
+    }
+}
